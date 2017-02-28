b5958dd6130abd10e2fc192cd4fa391a5af2d617
ntvinh11586 <ntvinh.11586@gmail.com>
2017-02-28 08:56:11 +0700

    Init project

---
+++ server.js
@@ -0,0 +1,15 @@
+'use strict';
+const express = require('express');
+const app = express();
+const chatCat = require('./app');
+
+app.set('port', process.env.PORT || 3000);
+app.use(express.static('public'));
+// app.set('views', './views'); // views = ./views
+app.set('view engine', 'ejs');
+
+app.use('/', chatCat.router);
+
+app.listen(app.get('port'), () => {
+  console.log('CHATCAT is running on Port: ', app.get('port'));
+});
