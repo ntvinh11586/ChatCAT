6feeb10598f3bafe2d4e39e28e19c9b6640b49b5
ntvinh11586 <ntvinh.11586@gmail.com>
2017-03-02 13:09:56 +0700

    Add socket.io - chatroom

--- app/socket/index.js
+++ app/socket/index.js
@@ -46,5 +46,10 @@ module.exports = (io, app) => {
       let room = h.removeUserFromRoom(allrooms, socket);
       socket.broadcast.to(room.roomID).emit('updateUsersList', JSON.stringify(room.users));
     });
+
+    // When a new message arrives
+    socket.on('newMessage', data => {
+      socket.broadcast.to(data.roomID).emit('inMessage', JSON.stringify(data));
+    });
   });
 }
